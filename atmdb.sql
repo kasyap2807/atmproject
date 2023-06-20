36e1922625a197ebf1f752672a850a814d16c032
kasyapkrishna <125851796+kasyap2807@users.noreply.github.com>
2023-06-20 21:48:15 +0530

    Update atmdb.sql

--- atmdb.sql
+++ atmdb.sql
@@ -2,15 +2,16 @@ use kittu;
 create table atmdb(
 acno varchar(10) primary key,
 Name varchar(20) not null,
-pin int unique,
+pin int,
 balence int);
-insert into atmdb values("ab10020001","k k kasyap",5321,32000),
+insert into atmdb values
+("ab10020000","10",1234,0);
+("ab10020001","k k kasyap",5321,32000),
 ("ab10020002","k hemasri",9999,45000),
 ("ab10020003","m p rajpaul",9027,33000),
 ("ab10020004","m d pranathi",2003,66000),
 ("ab10020005","m t c v sumaja",0708,28000);
 select * from atmdb;
-alter table atmdb drop constraint pin ;
-insert into atmdb values("ab10020000","ab10020016",1234,0);
-update atmdb set name="18" where acno="ab10020000";
+
+
 
